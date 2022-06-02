98dde4fcf47aef1c171d8db0fffcc510a31a89fb
godsakani <nyonggodwill11@gmail.com>
2022-06-02 18:09:55 +0100

    files: modified routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BordUserComponent } from './bord-user/bord-user.component';
+import { BordModComponent } from './bord-mod/bord-mod.component';
+import { BordAdminComponent } from './bord-admin/bord-admin.component';
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'user', component: BordUserComponent },
+  { path: 'mod', component: BordModComponent },
+  { path: 'admin', component: BordAdminComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
